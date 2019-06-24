d5fa64ea2787a477dbab24833a8e47e0b845557a
Tobias Eskou <5831278@RTS-365.dk>
2019-06-24 15:06:52 +0200

    almost

--- js/home.js
+++ js/home.js
@@ -1,3 +1,47 @@
 document.addEventListener("DOMContentLoaded", function() {
+    function valider(form) {
 
+        if (form.navn.value.length > 0) {
+            alert(form.navn.value)
+            document.getElementById("navn").style.background = "transparent";
+        } else {
+            alert("du har ikke skrevet et navn");
+            form.navn.focus();
+            document.getElementById("navn").style.background = "#ffaaaa";
+            return false;
+        }
+
+        if (form.email.value.length == 0) {
+            alert("du har ikke skrevet et email");
+            form.email.focus();
+            document.getElementById("email").style.background = "#ffaaaa";
+            return false;
+        } else {
+            if(validateEmail(form.email.value)){
+                alert(form.email.value)
+                document.getElementById("email").style.background = "transparent";
+            } else { 
+                alert("du har ikke skrevet en email");
+                form.email.focus();
+                document.getElementById("email").style.background = "#ffaaaa";
+                return false;
+            }
+            
+        }
+
+        function validateEmail(email){
+            let re = /(.+)@(.+){2,}\.(.+){2,}$/;
+            return re.test(String(email).toLowerCase());
+        }
+
+        if (form.besked.value.length > 0) {
+            alert(form.besked.value)
+            document.getElementById("besked").style.background = "transparent";
+        } else {
+            alert("du har ikke skrevet et besked");
+            form.besked.focus();
+            document.getElementById("besked").style.background = "#ffaaaa";
+            return false;
+        }
+}
 })
